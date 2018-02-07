e1a64fc88e6b4bef2071e3a2c9c12dce84af222f
rajayogan <rajayogan27@gmail.com>
2018-02-07 20:53:05 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+import { AngularFirestore } from 'angularfire2/firestore';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  rows;
+  columns;
+
+  constructor(private afs: AngularFirestore) {
+    this.getData();
+  }
+
+  getData() {
+    this.afs.collection('students').valueChanges().subscribe((students) => {
+      this.rows = students;
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+import { environment } from '../environments/environment';
+
+//Datatable
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.config),
+    AngularFirestoreModule,
+    NgxDatatableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
